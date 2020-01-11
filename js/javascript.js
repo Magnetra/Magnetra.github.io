ba2d974e7a46b3b23b22dc7d7e9493091615940f
DaltonJamie <59507959+Magnetra@users.noreply.github.com>
2020-01-11 02:32:49 -0500

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,3 @@
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+new Date() = document.lastModified('en-us', options)
+document.getElementById('futureDate').innerHTML = Date();
