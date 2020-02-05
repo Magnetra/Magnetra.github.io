8fba6cf30ac19446bd0227c9f500834db365623d
DaltonJamie <59507959+Magnetra@users.noreply.github.com>
2020-02-05 01:46:04 -0500

    Add files via upload

--- lesson5/js/toggleMenu.js
+++ lesson5/js/toggleMenu.js
@@ -1,11 +1,9 @@
 5
-//const options={minute: "numeric", hour: "numeric", day: "numeric", month: "long", }
+
 const options= {day: "long", day: "numeric", month: "long", year:"numeric"};
           
 document.getElementById('currentDate').textContent=new Date().toLocaleDateString("en-US", options);
-// const modoptions ={ second: "numeric", minute: "numeric", hour: "numeric", day: "numeric", month:"numeric", year: "numeric"};
-//document.getElementById('lastModified').textContent=new Date(document.lastModified);
-//document.getElementById('lastModified').textContent=document.lastModified;
+
 
 
 const hambutton = document.querySelector(".ham");
@@ -16,4 +14,11 @@ function toggleMenu() {
     document.querySelector(".navigation").classList.toggle("responsive");
 }
 
-
+function showBanner() {
+    var today= new Date();
+    var wkday= today.getDay();
+    if (wkday == 5){
+    document.getElementById("popup").classList.toggle("noshow");
+   
+}
+}
